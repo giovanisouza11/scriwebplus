2c99aefe164dd80ea45f80be28f9379c3b80df7e
giovanisouza11 <49683651+giovanisouza11@users.noreply.github.com>
2020-08-17 14:20:45 -0300

    Update appsimulador.js

--- public/js_sim/appsimulador.js
+++ public/js_sim/appsimulador.js
@@ -20,7 +20,7 @@ var ArrayLabel = [];
 var ArrayObjDinamic = [];
 var ArrayObjStatic =[];
 var simPath;
-var simPathInicial = 'http://scriweb.herokuapp.com/scriweb/simulacao/';
+var simPathInicial = '/scriweb/simulacao/';
 var FuncaoMatriz =[];
 var tempo = window.setInterval(AtualizaPorTempo, 500);
 var variavel;
