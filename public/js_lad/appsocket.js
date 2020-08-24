182a289d2b1d8ede84fc0898dd96c47517429915
giovanisouza11 <49683651+giovanisouza11@users.noreply.github.com>
2020-08-24 14:39:31 -0300

    Update appsocket.js

--- public/js_lad/appsocket.js
+++ public/js_lad/appsocket.js
@@ -33,7 +33,7 @@ function config(dado){
     	num_clp = dado;
     	context.font = '36pt Arial';
   	context.fillStyle = 'black';
-	context.filltext(num_clp, 100, 200);
+	context.filltext(parseStr(num_clp), 76, 227);
 }
 
 function Enviar(){
