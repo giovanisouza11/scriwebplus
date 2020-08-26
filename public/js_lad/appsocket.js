0a50a6ae75281ce97782677ea489a56c6c3abff8
giovanisouza11 <49683651+giovanisouza11@users.noreply.github.com>
2020-08-26 16:26:01 -0300

    Update appsocket.js

--- public/js_lad/appsocket.js
+++ public/js_lad/appsocket.js
@@ -23,10 +23,10 @@ socket.on('localizacao', function(data) {
 socket.on('config_retorno', function(data) {
        	num_clp = data;
 	context.fillStyle = 'ivory';
-  	context.fillRect(86, 220, 26, 20);
+  	context.fillRect(86, 240, 26, 20);
     	context.font = '32pt Arial';
   	context.fillStyle = 'red';
-	context.fillText(data, 86, 220);
+	context.fillText(data, 86, 240);
 });
 
 socket.on('saida', function(data) {
