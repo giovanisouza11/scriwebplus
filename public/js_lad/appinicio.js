6391001077066698a7f87e703fb913d990ae358a
Giovani Batista de Souza <49683651+giovanisouza11@users.noreply.github.com>
2020-09-05 11:19:39 -0300

    Update appinicio.js

--- public/js_lad/appinicio.js
+++ public/js_lad/appinicio.js
@@ -1,15 +1,15 @@
 var num_clp;
-//var Num_Linhas;
+var Num_Linhas;
 
 function draw_inicio() {
-	if (localStorange.num_clp > 0)
-		num_clp = localStorange.num_clp;
+	if (localStorage.num_clp > 0)
+		num_clp = localStorage.num_clp;
 	else
 		num_clp = 0;
-	//if (localStorange.num_linhas > 10 && localStorange.num_linhas<301)
-	//	Num_Linhas = localStorange.num_linhas;
-	//else
-	//	Num_Linhas = 100;		
+	if (localStorage.num_linhas > 10 && localStorage.num_linhas<301)
+		Num_Linhas = localStorage.num_linhas;
+	else
+		Num_Linhas = 100;		
 	draw_eletrico();
     	draw_botoes();
 	draw_ladder_inicio();	
