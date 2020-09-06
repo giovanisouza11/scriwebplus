cbcb0e56334af8e79a0eb99f09ced86a52995181
Giovani Batista de Souza <49683651+giovanisouza11@users.noreply.github.com>
2020-09-05 22:05:27 -0300

    Update appiniciosimulador.js

--- public/js_sim/appiniciosimulador.js
+++ public/js_sim/appiniciosimulador.js
@@ -1,17 +1,18 @@
 var num_clp;	    	
 function draw_inicio() {
 	if (localStorage.tela_largura>199 && localStorage.tela_largura<2001)
-		window.innerWidth = localStorage.tela_largura;
+		//window.innerWidth = localStorage.tela_largura;
 	else{
-		window.innerWidth = 1400;
+		//window.innerWidth = 1400;
 		localStorage.tela_largura = 1400;
 	}
 	if (localStorage.tela_altura>199 && localStorage.tela_altura<2001)
-  		window.innerHeight= localStorage.tela_altura;
+  		//window.innerHeight= localStorage.tela_altura;
 	else{
-		window.innerWidth =900;
+		//window.innerWidth =900;
 		localStorage.tela_altura = 900;
 	}
+	window.resizeTo(localStorage.tela_largura, localStorage.tela_altura);
 	if (localStorage.num_clp > 0)
 		num_clp = localStorage.num_clp;
 	else
