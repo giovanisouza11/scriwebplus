318e6fd1c61d354b2a4f4de8ef0b72446f8df082
Giovani Batista de Souza <49683651+giovanisouza11@users.noreply.github.com>
2020-09-05 22:08:31 -0300

    Update appbotaosim.js

--- public/js_sim/appbotaosim.js
+++ public/js_sim/appbotaosim.js
@@ -68,7 +68,8 @@ function tBotao() {
 			localStorage.setItem("num_clp", document.getElementById('input1').value);
 			localStorage.setItem("tela_largura", document.getElementById('input2').value);
 			localStorage.setItem("tela_altura", document.getElementById('input3').value);
-			draw_simulador_inicio();
+			window.resizeTo(localStorage.tela_largura, localStorage.tela_altura);
+			//draw_simulador_inicio();
 			//localStorage.num_clp = document.getElementById('input1').value;
 		}
 		// Get the <Apagar> element that closes the modal
