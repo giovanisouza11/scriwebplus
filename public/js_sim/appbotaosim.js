cb150f0415ca577dca8985c4906877791a83d157
Giovani Batista de Souza <49683651+giovanisouza11@users.noreply.github.com>
2020-09-15 15:24:22 -0300

    Update appbotaosim.js

--- public/js_sim/appbotaosim.js
+++ public/js_sim/appbotaosim.js
@@ -7,15 +7,15 @@ var simEdicao = 0;
 //desenha os botoes
 function draw_botoessim() {
     canvas1 = document.getElementById("tela1");
-	context1 = canvas1.getContext("2d");
+    context1 = canvas1.getContext("2d");
     canvas1.width = 75;
     canvas1.height = 570;
 
-	for(i=1; i<13; i++) {
+    for(i=1; i<13; i++) {
         if (valor_chave[i] === undefined)
-			valor_chave[i] = 0;
+		valor_chave[i] = 0;
         context1.drawImage(AImageB, 70*valor_chave[i]+(140*simEdicao), 45*(i-1), 70, 45, 5,(i-1)*47, 70, 45);
-	}
+    }
 }
 
 //Verifica QUAL botao foi acionado
@@ -31,17 +31,15 @@ function tBotao() {
 	}
 
 	if (valor_chave[12]==1){
-        window.open("/About", " About SCriWeb", "height=600,width=600");
-	    valor_chave[12]=0;
+    		window.open("/About", " About SCriWeb", "height=600,width=600");
+	    	valor_chave[12]=0;
 	}
 	if (valor_chave[11]==1){
-        window.open("/helpsim", "Help SCriWeb", "height=800,width=1000");
-	    valor_chave[11]=0;
+        	window.open("/helpsim", "Help SCriWeb", "height=800,width=1000");
+	    	valor_chave[11]=0;
 	}
 	if (valor_chave[10]==1){
-        //window.open("/configsim", "Config SCriWeb", "height=320,width=500");
-		//simArvore();
-		var modal = document.getElementById("myModal");
+        	var modal = document.getElementById("myModal");
 		var modalc = document.getElementById("myColuna");
 		var t_modal = modal.getElementsByTagName("h2");
 		t_modal[0].innerHTML = "Configuração Simulador";	
@@ -68,8 +66,6 @@ function tBotao() {
 			localStorage.setItem("num_clp", document.getElementById('input1').value);
 			localStorage.setItem("tela_largura", document.getElementById('input2').value);
 			localStorage.setItem("tela_altura", document.getElementById('input3').value);
-			//window.resizeTo(localStorage.tela_largura, localStorage.tela_altura);
-			//draw_simulador_inicio();
 		}
 		// Get the <Apagar> element that closes the modal
 		document.getElementsByClassName("apagar")[0].style.visibility = "hidden";
@@ -84,7 +80,7 @@ function tBotao() {
 			}
 		}
 
-	    valor_chave[10]=0;
+	    	valor_chave[10]=0;
 	}
 	if (simEdicao==0 && valor_chave[2]==1){
         	var inputCSV = document.createElement('input');
@@ -101,8 +97,8 @@ function tBotao() {
 		valor_chave[5]=1;
 	}
 	if (simEdicao==0 && valor_chave[3]==1){
-	    let texto = '0_id, 1_tipo, 2_nome, 3_var_1, 4_var_2, 5_pos_x_inicial, 6_dpos_x, 7_pos_x_final, 8_pos_y_inicial, 9_dpos_y, 10_pos_y_final, 11_inc_x1, 12_inc_x2, 13_inc_y1, 14_inc_y2, 15_var1_dependente, 16_var2_dependente, 17_piscar, 18_tempo_pisca, 19_figura, 20_funcao, 21_reserva, 22_reserva, 23_reserva, 24_reserva \n';
-	    for (var i=0; i<(ArrayObjStatic.length/20); i++) {
+	    	let texto = '0_id, 1_tipo, 2_nome, 3_var_1, 4_var_2, 5_pos_x_inicial, 6_dpos_x, 7_pos_x_final, 8_pos_y_inicial, 9_dpos_y, 10_pos_y_final, 11_inc_x1, 12_inc_x2, 13_inc_y1, 14_inc_y2, 15_var1_dependente, 16_var2_dependente, 17_piscar, 18_tempo_pisca, 19_figura, 20_funcao, 21_reserva, 22_reserva, 23_reserva, 24_reserva \n';
+	    	for (var i=0; i<(ArrayObjStatic.length/20); i++) {
 			texto += i +','+ArrayObjStatic[i*20+17]+',';
 			for (var j=0; j<17; j++)
 			    texto += ArrayObjStatic[(i*20)+j]+',';
@@ -115,10 +111,10 @@ function tBotao() {
 			//texto += ArrayObjDinamic[i*10+5] +',' + ArrayObjDinamic[i*10+6]+ ',,,,,';
 			texto += '\n';
 		}
-        let titulo ='SimScriWeb';
-        var blob = new Blob([texto], { type: "text/plain;charset=utf-8" });
-        saveAs(blob, titulo + ".csv");
-        valor_chave[3]=0;
+        	let titulo ='SimScriWeb';
+        	var blob = new Blob([texto], { type: "text/plain;charset=utf-8" });
+        	saveAs(blob, titulo + ".csv");
+        	valor_chave[3]=0;
 	}
 	if (simEdicao==0 && valor_chave[4]==1){
 		comandos = 1;
@@ -187,7 +183,6 @@ leitorDeCSV.addEventListener('load', leCSV);
 
 function pegaCSV(inputFile) {
 	var file = inputFile.files[0];
-			
 	leitorDeCSV.readAsText(file);
 }
 
