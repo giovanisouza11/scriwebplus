dd31f695f7c659bc305c9b5945309937519d53b2
Giovani Batista de Souza <49683651+giovanisouza11@users.noreply.github.com>
2020-09-22 09:27:32 -0300

    Update appbotaosim.js

--- public/js_sim/appbotaosim.js
+++ public/js_sim/appbotaosim.js
@@ -50,7 +50,7 @@ function tBotao() {
 	
 		modal.style.display = "block";
 		modalc.style.display = "none";
-		document.getElementById('input1').value = num_clp;
+		document.getElementById('input1').value = num_clp1;
 		document.getElementById('input2').value = localStorage.tela_largura;
 		document.getElementById('input3').value = localStorage.tela_altura;
 		// Get the <Close> element that closes the modal
@@ -63,7 +63,7 @@ function tBotao() {
 		var ok = document.getElementsByClassName("ok")[0];
 		ok.onclick = function() {
 			Config_Socket(document.getElementById('input1').value);
-			localStorage.setItem("num_clp", document.getElementById('input1').value);
+			//localStorage.setItem("num_clp", document.getElementById('input1').value);
 			localStorage.setItem("tela_largura", document.getElementById('input2').value);
 			localStorage.setItem("tela_altura", document.getElementById('input3').value);
 		}
