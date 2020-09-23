91e4d914c143c342ea4b67b550f0dd7979e5c2c5
Giovani Batista de Souza <49683651+giovanisouza11@users.noreply.github.com>
2020-09-23 14:41:29 -0300

    Update appiniciosimulador.js

--- public/js_sim/appiniciosimulador.js
+++ public/js_sim/appiniciosimulador.js
@@ -1,16 +1,14 @@
-var num_clp;	    	
+//var num_clp;	    	
 function draw_inicio() {
 	if (localStorage.tela_largura<200 || localStorage.tela_largura>2000)
 		localStorage.tela_largura = 1400;
 	if (localStorage.tela_altura<200 || localStorage.tela_altura>2000)
 		localStorage.tela_altura = 900;
 	//window.resizeTo(localStorage.tela_largura, localStorage.tela_altura);
-	if (localStorage.num_clp > 0)
-		num_clp = localStorage.num_clp;
-	else
-		num_clp = 0;
+	if (localStorage.num_clp1 <0 || localStorage.numclp1>30)
+		localStorage.setItem(num_clp, 0);
 	
 	draw_botoessim();
 	draw_simulador_inicio();
-	Config_Socket(num_clp);
+	Config_Socket(localStorage.num_clp1);
 }	
