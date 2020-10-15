bccc1d1762fbb5a601e3a8f083bfa680280582d0
Giovani Batista de Souza <49683651+giovanisouza11@users.noreply.github.com>
2020-10-15 10:27:05 -0300

    Update appbotaosim.js

--- public/js_sim/appbotaosim.js
+++ public/js_sim/appbotaosim.js
@@ -129,7 +129,7 @@ function tBotao() {
 		//inputCSV.accept = '*.csv';
 		//inputCSV.click();
 		//inputCSV.onchange = function() {
-	    		var file =  "/public/scriweb/simulacao/Elevador/Elevador.csv";
+	    		var file =  "/scriweb/simulacao/Elevador/Elevador.csv";
 			//var fileArr = file.target.result.split('\n');
 			//draw_processo(fileArr);	
 		
