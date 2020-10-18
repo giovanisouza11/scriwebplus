9b971b7169707bd3204545a0ad13e78f89ebefc4
Giovani Batista de Souza <49683651+giovanisouza11@users.noreply.github.com>
2020-10-18 11:54:37 -0300

    Update appeletrico.js

--- public/js_lad/appeletrico.js
+++ public/js_lad/appeletrico.js
@@ -36,30 +36,30 @@ function desenha_clp(){
 	context.font = '9pt Arial';
 	context.lineWidth = 1;
 	context.fillStyle = 'ivory';
-	context.fillRect(5, 110, 415, 220);
+	context.fillRect(5, 112, 415, 220);
 	context.fillStyle = 'black';
-	context.strokeRect(5, 110, 415, 220);
+	context.strokeRect(5, 112, 415, 220);
 	context.lineWidth = 0.3;
-	context.strokeRect(5, 130, 415, 40);
-	context.strokeRect(5, 270, 415, 40);
+	context.strokeRect(5, 132, 415, 40);
+	context.strokeRect(5, 272, 415, 40);
 	context.lineWidth = 1;
 	for(var i=0; i<13; i++)
 		desenha_borne_clp(i);
 	context.font = '9pt Arial';
 
 	context.fillStyle = 'red';
-	context.fillRect(27, 201, 32, 18);
+	context.fillRect(27, 203, 32, 18);
 	context.fillStyle = 'black';
-	context.strokeRect(25, 199, 36, 22);
-	context.fillText('Power', 25, 193);
-	context.strokeRect(25, 239, 36, 22);
-	context.fillText('Run', 31, 233);
-	context.fillText('Entradas - I0.', 86, 182);
-	context.fillText('Memórias - M0.', 76, 199);
-	context.fillText('M1.', 139, 209);
-	context.fillText('M2.', 139, 219);
-	context.fillText('Dervação - TR', 78, 245);
-	context.fillText('Saídss  - Q0.', 90, 263);
+	context.strokeRect(25, 201, 36, 22);
+	context.fillText('Power', 25, 195);
+	context.strokeRect(25, 241, 36, 22);
+	context.fillText('Run', 31, 235);
+	context.fillText('Entradas - I0.', 86, 184);
+	context.fillText('Memórias - M0.', 76, 201);
+	context.fillText('M1.', 139, 211);
+	context.fillText('M2.', 139, 221);
+	context.fillText('Dervação - TR', 78, 247);
+	context.fillText('Saídss  - Q0.', 90, 265);
 	liga_led_CLP();
 
 
@@ -68,24 +68,24 @@ function desenha_clp(){
 function desenha_borne_clp(i){
 	context.lineWidth = 1;
 	context.fillStyle = 'black';
-	context.strokeRect(10+(i*32), 130, 16, 20);
-	context.strokeRect(10+(i*32), 290, 16, 20);
+	context.strokeRect(10+(i*32), 132, 16, 20);
+	context.strokeRect(10+(i*32), 292, 16, 20);
 	context.lineWidth = 0.3;
-	context.strokeRect(8+(i*32), 310, 16, 20);
-	context.strokeRect(8+(i*32), 110, 16, 20);
+	context.strokeRect(8+(i*32), 312, 16, 20);
+	context.strokeRect(8+(i*32), 112, 16, 20);
 	context.lineWidth = 0.5;
 	context.shadowColor = 'black';
 	context.shadowOffsetX = 1;
 	context.shadowOffsetY = 1;
-	context.strokeRect(10+(i*32), 113, 14, 14);
-	context.strokeRect(10+(i*32), 313, 14, 14);
+	context.strokeRect(10+(i*32), 115, 14, 14);
+	context.strokeRect(10+(i*32), 315, 14, 14);
 	context.beginPath();
-	context.ellipse(17+(i*32), 120, 4, 4, 0, 0, 359);
-	context.ellipse(17+(i*32), 120, 4, 2, i*30, 0, 359);
+	context.ellipse(17+(i*32), 122, 4, 4, 0, 0, 359);
+	context.ellipse(17+(i*32), 122, 4, 2, i*30, 0, 359);
 	context.stroke();
 	context.beginPath();
-	context.ellipse(17+(i*32), 320, 4, 4, 0, 0, 359);
-	context.ellipse(17+(i*32), 320, 4, 2, i*30, 0, 359);
+	context.ellipse(17+(i*32), 322, 4, 4, 0, 0, 359);
+	context.ellipse(17+(i*32), 322, 4, 2, i*30, 0, 359);
 	context.stroke();
 	context.lineWidth = 1;
 	context.shadowOffsetX = 0;
@@ -93,12 +93,12 @@ function desenha_borne_clp(i){
 	context.font = '7pt Arial';
 
 	if (i==0){
-		context.fillText('COM', 6, 165);
-		context.fillText('COM', 6, 285);
+		context.fillText('COM', 6, 167);
+		context.fillText('COM', 6, 287);
 	}
 	else {
-		context.fillText('I0.'+(i-1), 8+(i*32), 165);
-		context.fillText('Q0.'+(i-1), 8+(i*32), 285);
+		context.fillText('I0.'+(i-1), 8+(i*32), 167);
+		context.fillText('Q0.'+(i-1), 8+(i*32), 287);
 	}
 }
 
@@ -135,26 +135,26 @@ function trocar() {
 function run_CLP(){
 	if(comandos == 1) {
 		context.fillStyle = 'green';
-		context.fillRect(27, 241, 32, 18);
+		context.fillRect(27, 243, 32, 18);
 		context.font = '16pt Arial';
 		context.fillStyle = 'black';
-		context.fillText('A', 36, 259);
+		context.fillText('A', 36, 261);
 		liga_led_CLP();
 	}
 	if(comandos == 2) {
 		context.fillStyle = 'green';
-		context.fillRect(27, 241, 32, 18);
+		context.fillRect(27, 243, 32, 18);
 		context.font = '16pt Arial';
 		context.fillStyle = 'black';
-		context.fillText('1', 36, 259);
+		context.fillText('1', 36, 261);
 		liga_led_CLP();
 	}
 	if(comandos == 3) {
 		context.fillStyle = 'green';
-		context.fillRect(27, 241, 32, 18);
+		context.fillRect(27, 243, 32, 18);
 		context.font = '16pt Arial';
 		context.fillStyle = 'black';
-		context.fillText('P', 36, 259);
+		context.fillText('P', 36, 261);
 		liga_led_CLP();
 	}
 }
@@ -165,7 +165,7 @@ function liga_led_CLP(){
 			context.fillStyle = 'green';
 		else
 			context.fillStyle = 'white';
-		context.fillRect(45+(i*32), 132, 10, 16);
+		context.fillRect(45+(i*32), 134, 10, 16);
 	}
 	context.font = '9pt Arial';
 
@@ -175,34 +175,34 @@ function liga_led_CLP(){
 			context.fillStyle = 'green';
 		else
 			context.fillStyle = 'gray';
-		context.fillRect(403-(i*16), 177, 14, 5);
-		context.fillText(i, j+403-(i*16), 192);
+		context.fillRect(403-(i*16), 179, 14, 5);
+		context.fillText(i, j+403-(i*16), 194);
 		if (M[i]==1)
 			context.fillStyle = 'green';
 		else
 			context.fillStyle = 'gray';
-		context.fillRect(403-(i*16), 194, 14, 5);
+		context.fillRect(403-(i*16), 196, 14, 5);
 		if (M[i+16]==1)
 			context.fillStyle = 'green';
 		else
 			context.fillStyle = 'gray';
-		context.fillRect(403-(i*16), 203, 14, 5);
+		context.fillRect(403-(i*16), 205, 14, 5);
 		if (M[i+32]==1)
 			context.fillStyle = 'green';
 		else
 			context.fillStyle = 'gray';
-		context.fillRect(403-(i*16), 213, 14, 5);
+		context.fillRect(403-(i*16), 215, 14, 5);
 		if (R[i]==1)
 			context.fillStyle = 'green';
 		else
 			context.fillStyle = 'gray';
-		context.fillRect(403-(i*16), 240, 14, 5);
+		context.fillRect(403-(i*16), 242, 14, 5);
 		if (Q[i]==1)
 			context.fillStyle = 'green';
 		else
 			context.fillStyle = 'gray';
-		context.fillRect(403-(i*16), 258, 14, 5);
-		context.fillText(i, j+403-(i*16), 256);
+		context.fillRect(403-(i*16), 260, 14, 5);
+		context.fillText(i, j+403-(i*16), 258);
 	}
 	if(comandos != 0) {
 		for(var i=0; i <12; i++){
@@ -210,9 +210,9 @@ function liga_led_CLP(){
 				context.fillStyle = 'green';
 			else
 				context.fillStyle = 'white';
-			context.fillRect(45+(i*32), 292, 10, 16);
+			context.fillRect(45+(i*32), 294, 10, 16);
 
-			context.drawImage(AImage1, (AImage1.width/5)+(AImage1.width/5)*Q[i], 110, AImage1.width/5, 110, 33+i*32, 330, AImage1.width/5, 110);
+			context.drawImage(AImage1, (AImage1.width/5)+(AImage1.width/5)*Q[i], 110, AImage1.width/5, 110, 33+i*32, 333, AImage1.width/5, 110);
 		}
 	}
 
