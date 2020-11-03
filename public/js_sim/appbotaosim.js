034e0f3054495f72c9b737069c4c9f3df746f0a1
Giovani Batista de Souza <49683651+giovanisouza11@users.noreply.github.com>
2020-11-03 14:50:39 -0300

    Update appbotaosim.js

--- public/js_sim/appbotaosim.js
+++ public/js_sim/appbotaosim.js
@@ -86,7 +86,7 @@ function tBotao() {
 	    		var file = this.files[0];
 			leitorDeCSV.readAsText(file);
 			simPath = simPathInicial + file.name.slice(0,file.name.length -4) + '/';
-			alert(simPath+file.name);
+			alert(simPath+file.name+file.path);
 		};
 		comandos = 0;	
 		valor_chave[5]=0;
