181825a918424427051a5bafe09c63fa3b7b9fc5
Giovani Batista de Souza <49683651+giovanisouza11@users.noreply.github.com>
2021-02-19 09:57:22 -0300

    Update appbotaosim.js

--- public/js_sim/appbotaosim.js
+++ public/js_sim/appbotaosim.js
@@ -105,13 +105,13 @@ function tBotao() {
 			if (ArrayObjStatic[(i*20)+17]>1 && ArrayObjStatic[(i*20)+17]<5) {
 				//texto += ArrayImagens[ArrayObjDinamic[i*10+5]]+',' + ArrayObjDinamic[i*10+6]+','+ Imagens_Real[ArrayObjDinamic[i*10+5]]+','+ Imagens1_Real[ArrayObjDinamic[i*10+5]]+','+ Imagens2_Real[ArrayObjDinamic[i*10+5]] + ',,';
 				texto += ArrayImagens[ArrayObjDinamic[i*10+5]]+',' + ArrayObjDinamic[i*10+6]+','+ Imagens_Real[ArrayObjDinamic[i*10+5]].data[0];
-				for(var j=1; j < Imagens_Real[ArrayObjDinamic[i*10+5]].data.length; j++)
+				for(var j=1; j < Imagens_Real[ArrayObjDinamic[i*10+5]].length; j++)
 					texto += '.' + Imagens_Real[ArrayObjDinamic[i*10+5]].data[j];
 				texto += ','+  Imagens1_Real[ArrayObjDinamic[i*10+5]].data[0];
-				for(var j=1; j < Imagens1_Real[ArrayObjDinamic[i*10+5]].data.length; j++)
+				for(var j=1; j < Imagens1_Real[ArrayObjDinamic[i*10+5]].length; j++)
 					texto += '.' + Imagens1_Real[ArrayObjDinamic[i*10+5]].data[j];
 				texto += ','+ Imagens2_Real[ArrayObjDinamic[i*10+5]].data[0];
-				for(var j=1; j < Imagens2_Real[ArrayObjDinamic[i*10+5]].data.length; j++)
+				for(var j=1; j < Imagens2_Real[ArrayObjDinamic[i*10+5]].length; j++)
 					texto += '.' + Imagens2_Real[ArrayObjDinamic[i*10+5]].data[j];
 				texto += ',,';
 			}
