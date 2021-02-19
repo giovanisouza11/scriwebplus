3c053b96d0089fa0e094c58e1eea9f61c12cbe97
Giovani Batista de Souza <49683651+giovanisouza11@users.noreply.github.com>
2021-02-19 11:03:51 -0300

    Update appbotaosim.js

--- public/js_sim/appbotaosim.js
+++ public/js_sim/appbotaosim.js
@@ -104,18 +104,19 @@ function tBotao() {
 			    texto += ArrayObjStatic[(i*20)+j]+',';
 			if (ArrayObjStatic[(i*20)+17]>1 && ArrayObjStatic[(i*20)+17]<5) {
 				//texto += ArrayImagens[ArrayObjDinamic[i*10+5]]+',' + ArrayObjDinamic[i*10+6]+','+ Imagens_Real[ArrayObjDinamic[i*10+5]]+','+ Imagens1_Real[ArrayObjDinamic[i*10+5]]+','+ Imagens2_Real[ArrayObjDinamic[i*10+5]] + ',,';
+				
 				texto += ArrayImagens[ArrayObjDinamic[i*10+5]]+',' + ArrayObjDinamic[i*10+6]+','+ Imagens_Real[ArrayObjDinamic[i*10+5]].data[0];
 				for(var j=1; j < Imagens_Real[ArrayObjDinamic[i*10+5]].data.length; j++)
 					texto += '.' + Imagens_Real[ArrayObjDinamic[i*10+5]].data[j];
 				//texto += ','+ Imagens1_Real[ArrayObjDinamic[i*10+5]]+','+ Imagens2_Real[ArrayObjDinamic[i*10+5]] + ',,';
-				if (Imagens1_Real[ArrayObjDinamic[i*10+5]] != '') {
+				if (Imagens1_Real[ArrayObjDinamic[i*10+5]] == (object ImageData)) {
 					texto += ','+  Imagens1_Real[ArrayObjDinamic[i*10+5]].data[0];
 					for(var j=1; j < Imagens1_Real[ArrayObjDinamic[i*10+5]].data.length; j++)
 						texto += '.' + Imagens1_Real[ArrayObjDinamic[i*10+5]].data[j];
 				}
 				else
 					texto += ',';
-				if (Imagens2_Real[ArrayObjDinamic[i*10+5]] != '') {
+				if (Imagens2_Real[ArrayObjDinamic[i*10+5]] == (object ImageData)) {
 					texto += ','+ Imagens2_Real[ArrayObjDinamic[i*10+5]].data[0];
 					for(var j=1; j < Imagens2_Real[ArrayObjDinamic[i*10+5]].data.length; j++)
 						texto += '.' + Imagens2_Real[ArrayObjDinamic[i*10+5]].data[j];
