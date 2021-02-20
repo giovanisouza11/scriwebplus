7cb9ac2f7720830c7fb4d4f1ca31488d8905d653
Giovani Batista de Souza <49683651+giovanisouza11@users.noreply.github.com>
2021-02-20 14:48:24 -0300

    Update appbotaosim.js

--- public/js_sim/appbotaosim.js
+++ public/js_sim/appbotaosim.js
@@ -106,15 +106,15 @@ function tBotao() {
 				//texto += ArrayImagens[ArrayObjDinamic[i*10+5]]+',' + ArrayObjDinamic[i*10+6]+','+ Imagens_Real[ArrayObjDinamic[i*10+5]]+','+ Imagens1_Real[ArrayObjDinamic[i*10+5]]+','+ Imagens2_Real[ArrayObjDinamic[i*10+5]] + ',,';
 				
 				texto += ArrayImagens[ArrayObjDinamic[i*10+5]]+',' + ArrayObjDinamic[i*10+6]+','; //+ Imagens_Real[ArrayObjDinamic[i*10+5]].data[0];
-				if ( Array.isArray(Imagens_Real[ArrayObjDinamic[i*10+5]].data) == true)
+				if ( Array.isArray(Imagens_Real[ArrayObjDinamic[i*10+5]]) == true)
 					texto += Imagens_Real[ArrayObjDinamic[i*10+5]].data.join('.');
 				else
 					texto += ',';
-				if ( Array.isArray(Imagens1_Real[ArrayObjDinamic[i*10+5]].data) == true)
+				if ( Array.isArray(Imagens1_Real[ArrayObjDinamic[i*10+5]]) == true)
 					texto += Imagens1_Real[ArrayObjDinamic[i*10+5]].data.join('.');
 				else
 					texto += ',';
-				if ( Array.isArray(Imagens2_Real[ArrayObjDinamic[i*10+5]].data) == true)
+				if ( Array.isArray(Imagens2_Real[ArrayObjDinamic[i*10+5]]) == true)
 					texto += Imagens2_Real[ArrayObjDinamic[i*10+5]].data.join('.');
 				texto += ',,';
 				
