5e52edb3a606e531d1257e59633e43d65404ed6e
Giovani Batista de Souza <49683651+giovanisouza11@users.noreply.github.com>
2021-03-04 15:28:24 -0300

    Update appsimfuncao.js

--- public/js_sim/appsimfuncao.js
+++ public/js_sim/appsimfuncao.js
@@ -442,7 +442,7 @@ var leitorDePNG = new FileReader();
 leitorDePNG.addEventListener('load', lePNG);
 
 function lePNG(evt) {
-	Imagens_Real[ArrayObjDinamic[(index_var_config)*10+5]] = evt.target.result;
+	Imagens_Real[ArrayObjDinamic[(index_var_config)*10+5]] = evt.target.result.split('.');
 	confirm(readAsBinaryString(file));
 			
 	//draw_processo(fileArr);
