515a990bb7afd707a833abf91523329f49639833
Giovani Batista de Souza <49683651+giovanisouza11@users.noreply.github.com>
2021-03-24 19:37:56 -0300

    Update appsocket.js

--- public/js_lad/appsocket.js
+++ public/js_lad/appsocket.js
@@ -56,3 +56,27 @@ function envia_entrada(data){
 }
 
 
+socket.on('config_socket_r', function(data) {
+	localStorage.setItem("num_clp1", data);
+});
+function Enviar_Entrada(){
+	socket.emit('entradax', I.join()+ ','+  localStorage.num_clp1);
+}
+function Config_Socket(dado){
+	socket.emit('sup', dado);
+}
+
+/*function envia_entrada(data){
+	while (data.length < I.length) {
+		data[data.length] = I[data.length];
+	}
+     	socket.emit('entradax', data.join()+ ','+  localStorage.num_clp1);
+}
+*/
+function envia_memoria(data){
+	//while (data.length < M.length) {
+	//	data[data.length] = M[data.length];
+	//}
+     	socket.emit('memoriax', data+','+enderecoCT(data,0)+ ','+  localStorage.num_clp1);
+}
+
