01b2629ec630c83faf0192c2046529e7fd49f359
Giovani Batista de Souza <49683651+giovanisouza11@users.noreply.github.com>
2021-03-26 14:36:14 -0300

    Update appfuncao.js

--- public/js_lad/appfuncao.js
+++ public/js_lad/appfuncao.js
@@ -2,16 +2,16 @@
 function ladder_simulador(){
 	var ladder = document.getElementById('IdLadder');
 	var simul = document.getElementById('IdSimul');
-	if (simul.style.display === "block") {
+	if (ladder.style.display === "block") {
+		ladder.style.display = "none";
+		simul.style.display = "block";
+		draw_botoessim();
+	}
+	else {
 		ladder.style.display = "block";
 		simul.style.display = "none";
 		draw_eletrico();
     		draw_botoes();
-	}
-	else {
-		ladder.style.display = "none";
-		simul.style.display = "block";
-		draw_botoessim();
 	}
 }
 
