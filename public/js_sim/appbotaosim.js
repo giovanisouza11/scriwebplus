a7279a63a85c1ef5d0ea5f0f1c0a74ee483428d8
Giovani Batista de Souza <49683651+giovanisouza11@users.noreply.github.com>
2021-03-30 16:51:28 -0300

    Update appbotaosim.js

--- public/js_sim/appbotaosim.js
+++ public/js_sim/appbotaosim.js
@@ -214,8 +214,16 @@ function tBotao() {
 //leitura de arquivos
 //Fonte https://tableless.com.br/file-api-trabalhando-com-arquivos-locais-usando-javascript/
 var Sim_LeitorDeCSV = new FileReader();
-Sim_LeitorDeCSV.addEventListener('load', Sim_leCSV);
-
+//Sim_LeitorDeCSV.addEventListener('load', Sim_leCSV);
+Sim_LeitorDeCSV.onLoad = function(evt){
+	var fileArr = evt.target.result.split('\n');
+	if (simulacao_set_inicial == 0) {
+		draw_simulador_inicio();
+	}
+	alert("entrou");
+	Sim_draw_processo(fileArr);
+}
+	
 function pegaCSV2(inputFile) {
 	var file = inputFile.files[0];
 	Sim_LeitorDeCSV.readAsText(file);
