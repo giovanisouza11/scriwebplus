5bc37c90e7f9979d85376be6bbf3642f2872abb2
Giovani Batista de Souza <49683651+giovanisouza11@users.noreply.github.com>
2021-04-01 13:44:11 -0300

    Update appsocket.js

--- public/js_lad/appsocket.js
+++ public/js_lad/appsocket.js
@@ -3,24 +3,24 @@ var socket = io(); //('http://192.168.0.100:4333');
 socket.emit('connect', num_clp);
 socket.on('memoria', function(data) {
 	M = data.split(',');
-	Sim_M = data.split(',');
+	//Sim_M = data.split(',');
 });
 socket.on('timer', function(data) {
 	T = data.split(',');
-	Sim_T = data.split(',');
+	//Sim_T = data.split(',');
 });
 socket.on('counter', function(data) {
 	C = data.split(',');
-	Sim_C = data.split(',');
+	//Sim_C = data.split(',');
 
 });
 socket.on('entrada', function(data) {
        	I = data.split(',');
-	Sim_I = data.split(',');
+	//Sim_I = data.split(',');
 });
 socket.on('tr', function(data) {
        R = data.split(',');
-       Sim_R = data.split(',');	
+       //Sim_R = data.split(',');	
 });
 socket.on('localizacao', function(data) {
        localizacao = data;
@@ -36,7 +36,7 @@ socket.on('config_retorno', function(data) {
 
 socket.on('saida', function(data) {
        	Q = data.split(',');
-	Sim_Q = data.split(',');
+	//Sim_Q = data.split(',');
 	liga_led_CLP();
 	if (comandos != 0) {
 		monitora_ladder();
@@ -49,7 +49,7 @@ function config(dado){
 
 function Enviar(){
 	var data = I.join()+ ','+ num_clp;
-    	Sim_I = I;
+    	//Sim_I = I;
 	socket.emit('programax', programa+ ','+ num_clp);
     	socket.emit('comandosx', comando+ ','+ num_clp);
     	socket.emit('entradax',data);
@@ -59,7 +59,7 @@ function Envia_Entrada_Ele(data){
 	while (data.length < I.length) {
 		data[(I.length)-1] = I[(I.length)-1];
 	}
-	Sim_I = data.split(',');
+	//Sim_I = data.split(',');
 	socket.emit('entradax', data.join()+ ','+ num_clp);
 }
 
@@ -67,7 +67,7 @@ function Envia_Entrada_S(data){
 	while (data.length < Sim_I.length) {
 		data[data.length] = Sim_I[data.length];
 	}
-	Sim_I = data.split(',');
+	//Sim_I = data.split(',');
 	socket.emit('entradax', data.join()+ ','+ num_clp);
 }
 
