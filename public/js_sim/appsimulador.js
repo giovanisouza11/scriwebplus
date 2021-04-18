0ccf7281306a83551217f456e1444b1bb174b1b1
Giovani Batista de Souza <49683651+giovanisouza11@users.noreply.github.com>
2021-04-18 14:29:08 -0300

    Update appsimulador.js

--- public/js_sim/appsimulador.js
+++ public/js_sim/appsimulador.js
@@ -376,8 +376,8 @@ function Desenha_Ihm_Sim() {
         			n_posicao = (n_linhas * 4) + posicao;
       			}
       			if(funcao==3) {
-        			Sim_Context.fillText(tipo_funcao[funcao]+(n_linhas*3), 190,linha);
         			n_posicao = (n_linhas * 3) + posicao;
+				Sim_Context.fillText(tipo_funcao[funcao]+n_posicao, 190,linha);
       			}
       			do {
         			switch (parseInt(funcao)){
