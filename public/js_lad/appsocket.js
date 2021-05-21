776e6bc184aacb706e5c45cd547e52da73f643e3
Giovani Batista de Souza <49683651+giovanisouza11@users.noreply.github.com>
2021-05-21 16:04:26 -0300

    Update appsocket.js

--- public/js_lad/appsocket.js
+++ public/js_lad/appsocket.js
@@ -1,56 +1,6 @@
-//var socket = io(); //('http://192.168.0.100:4333');
-//var num_clp = 0;
-//socket.emit('connect', num_clp);
-//socket.on('memoria', function(data) {
-//	M = data.split(',');
-	//Sim_M = data.split(',');
-//});
-//socket.on('timer', function(data) {
-//	T = data.split(',');
-	//Sim_T = data.split(',');
-//});
-//socket.on('counter', function(data) {
-//	C = data.split(',');
-	//Sim_C = data.split(',');
-
-//});
-//socket.on('entrada', function(data) {
-  //     	I = data.split(',');
-	//Sim_I = data.split(',');
-//});
-//socket.on('tr', function(data) {
-  //     R = data.split(',');
-       //Sim_R = data.split(',');	
-//});
-//socket.on('localizacao', function(data) {
-  //     localizacao = data;
-//});
-//socket.on('config_retorno', function(data) {
-  //     	num_clp = data;
-	//context.fillStyle = 'ivory';
- // 	context.fillRect(86, 202, 45, 31);
-   // 	context.font = '32pt Arial';
-  //	context.fillStyle = 'red';
-	//context.fillText(data, 86, 233);
-//});
-
-//socket.on('saida', function(data) {
-  //     	Q = data.split(',');
-	//Sim_Q = data.split(',');
-	//liga_led_CLP();
-	//if (comandos != 0) {
-	//	monitora_ladder();
-	//};
-//});
-
-//function config(dado){
-  //  	socket.emit('clp', dado);
-//}
-
 function Enviar(){
 	clpI = I;
-    	//Sim_I = I;
-	clp_programa = programa;
+	clp_programa = booleano;
     	clp_comandos = comando;
 }
 
