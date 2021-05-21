15fa6fbd88cdec58307fc822da8fb76429c41c78
Giovani Batista de Souza <49683651+giovanisouza11@users.noreply.github.com>
2021-05-21 16:07:23 -0300

    Update appsocket.js

--- public/js_lad/appsocket.js
+++ public/js_lad/appsocket.js
@@ -1,7 +1,7 @@
 function Enviar(){
 	clpI = I;
 	clp_programa = booleano;
-    	clp_comandos = comando;
+    	clp_comandos = comandos;
 }
 
 function Envia_Entrada_Ele(data){
@@ -17,18 +17,10 @@ function Envia_Entrada_S(data){
 	while (data.length < I.length) {
 		data[(I.length)-1] = I[(I.length)-1];
 	}
-	//Sim_I = data.split(',');
 	if (Tela_Eletrico_Simulador==1)
 		clpI = data;
 }
 
-//socket.on('config_socket_r', function(data) {
-//	localStorage.setItem("num_clp1", data);
-//});
-
-//function Sim_Config_Socket(dado){
-//    	socket.emit('clp', dado);
-//}
 function Envia_Memoria_S(data){
     	//socket.emit('memoriax', data+','+Sim_enderecoCT(data,0)+ ','+ num_clp); 
 	clpM[data.substr(1)] = Sim_enderecoCT(data,0);
