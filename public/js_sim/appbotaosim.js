467c7bdfe0321cb9923fd742bdf9fc533400ff11
Giovani Batista de Souza <49683651+giovanisouza11@users.noreply.github.com>
2021-05-27 09:35:28 -0300

    Update appbotaosim.js

--- public/js_sim/appbotaosim.js
+++ public/js_sim/appbotaosim.js
@@ -91,7 +91,7 @@ function Sim_Botao_Click() {
 		var t_modal = modal1.getElementsByTagName("h2");
 		t_modal[0].innerHTML = "Download";	
 		var modalb1 = document.getElementById("myBody1");
-		modalb1.innerHTML = "<p><a href='http://scriweb.herokuapp.com/ftp/Alarme/Alarme.csv'>Alarmes</a></p>";
+		modalb1.innerHTML = "<p><a href='http://scriweb.herokuapp.com/ftp/Alarme/Alarme.csv' onclick='Sim_Le_Arquivo_Nuvem()'>Alarmes</a></p>";
 		modalb1.innerHTML += "<p><a href='http://scriwebshow.herokuapp.com/ftp/Classificacao/Classificacao.csv'>Classificação</a></p>";
 		modalb1.innerHTML += "<p><a href='http://scriwebshow.herokuapp.com/ftp/Corte_Vinco/Corte_Vinco.csv'>Corte e Vinco</a></p>";
 		modalb1.innerHTML += "<p><a href='http://scriwebshow.herokuapp.com/ftp/Elevador/Elevador.csv'>Elevador(3 andares)</a></p>";
@@ -189,5 +189,8 @@ function Sim_Le_Arquivo_Nuvem() {
 		Sim_Leitor_Arquivo.readAsText(file);
 		Sim_Path = Sim_PathInicial + file.name.slice(0,file.name.length -4) + '/';
 		titulo = file.name.slice(0,file.name.length -4);
+	modal1.style.display = "none";
+	
+	
 	//};
 }
