8d29a0592586b3fd0465288f7ce50b516784ced3
Giovani Batista de Souza <49683651+giovanisouza11@users.noreply.github.com>
2021-11-04 19:46:36 -0300

    Update appsimulador.js

--- public/js_sim/appsimulador.js
+++ public/js_sim/appsimulador.js
@@ -118,7 +118,7 @@ function eInputS(event) {
 			Sim_Escreve_CT(ArrayObjStatic[variavel*20+1],0,0);
 
 		ICampo.style.left = "10px";
-		ICampo.style.top = '1100px";
+		ICampo.style.top = '1100px';
 		CInput.blur();
 		ICampo.style.display = "none";
 	}
