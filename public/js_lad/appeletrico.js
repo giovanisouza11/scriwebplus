9aa71ae31e0be4a29bc4b0cf6adaf856e20ad010
Giovani Batista de Souza <49683651+giovanisouza11@users.noreply.github.com>
2021-11-05 12:10:44 -0300

    Update appeletrico.js

--- public/js_lad/appeletrico.js
+++ public/js_lad/appeletrico.js
@@ -20,7 +20,7 @@ function draw_eletrico() {
 	Ihm_Context =  Ihm_Canvas.rtgetContext("2d");
     	Ihm_Canvas.width = 700;
     	Ihm_Canvas.height = 300;
-	Ihm_Canvas.top = 500;
+	Ihm_Canvas.top = 700;
     	//Canvas eletrico
 	canvas = document.getElementById("tela1");
 	context= canvas.getContext("2d");
