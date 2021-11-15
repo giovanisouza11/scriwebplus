7ff74d4f4d5821f6aaaa94a1ccbda35ec4a4c087
Giovani Batista de Souza <49683651+giovanisouza11@users.noreply.github.com>
2021-11-15 12:28:32 -0300

    Update appbotaosim.js

--- public/js_sim/appbotaosim.js
+++ public/js_sim/appbotaosim.js
@@ -210,10 +210,11 @@ function Sim_Le_Arquivo_Nuvem() {
 		titulo = file.name.slice(0,file.name.length -4);
 	};*/
 	var arquivo = 'Alarme';
-	var file = 'http://scriwebplus.herokuapp.com/ftp/'+arquivo+'/'+arquivo+'.csv';
+	var file = this.files[0];
+	file.name = 'http://scriwebplus.herokuapp.com/ftp/'+arquivo+'/'+arquivo+'.csv';
 	alert(arquivo);
     	//var file = arquivo+'.csv';
-	Sim_Leitor_Arquivo.readAsText(file);
+	Sim_Leitor_Arquivo.readAsText(inputCSV.value);
 	alert(arquivo);
 	Sim_Path = Sim_PathInicial + file.name.slice(0,file.name.length -4) + '/';
 	titulo = file.name.slice(0,file.name.length -4);
