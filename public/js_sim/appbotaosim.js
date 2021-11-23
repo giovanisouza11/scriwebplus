3ae2b974619af1931c2b2df680ca1791af646799
Giovani Batista de Souza <49683651+giovanisouza11@users.noreply.github.com>
2021-11-23 11:23:56 -0300

    Update appbotaosim.js

--- public/js_sim/appbotaosim.js
+++ public/js_sim/appbotaosim.js
@@ -82,10 +82,10 @@ function Sim_Botao_Click() {
 		}
 
         	//let titulo ='SimScriWeb';
-        	var blob = new Blob([texto], { type: "text/plain;charset=utf-8" });
-		saveAs(blob, titulo + ".csv");
+        	/*var blob = new Blob([texto], { type: "text/plain;charset=utf-8" });
+		saveAs(blob, titulo + ".csv");*/
    	   	Sim_Botao_Funcao[3]=0;
-		/*var zip = new JSZip();
+		var zip = new JSZip();
 		var img = zip.folder(titulo);
 		//zip.folder.(Sim_Path)
 		zip.folder(img).forEach(function (relativePath, file){
@@ -94,10 +94,12 @@ function Sim_Botao_Click() {
 		});
 		var aux_titulo = titulo+".csv";
 		img.file(aux_titulo, texto);
+		for( var aux1= 0; aux1 < Imagens.length; aux1++)
+			img.file(Imagens[aux1].name, Imagens[aux1]); 
 		aux_titulo = titulo+".zip";
 		zip.generateAsync({type:"blob"}).then(function(content) {
     			saveAs(content, aux_titulo);
-		});*/
+		});
         }
 	if (Sim_Edicao==0 && Sim_Botao_Funcao[5]==1){
 		Sim_Botao_Funcao[5]=0;
