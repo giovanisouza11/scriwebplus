ea0c819a5104dc51debeb045e519c75a2abdbf5b
Giovani Batista de Souza <49683651+giovanisouza11@users.noreply.github.com>
2021-12-04 08:48:59 -0300

    Create appcompilasfc.js

---
+++ public/js_sfc/appcompilasfc.js
@@ -0,0 +1,18 @@
+//-----------------------------------------------------------------------------------
+//Vari�veis utilizadas na convers�o
+//-----------------------------------------------------------------------------------
+
+//=====================================================================================
+// Converte programa LADDER -> BOOLEANO
+// A cada deriva��o vertical coloca uma fun��o SET
+// Na primeira liga��o horizontal da linha, tamb�m coloco fun��o SET, despreza as outras
+//======================================================================================
+function compila_sfc(){
+	
+}
+
+//--------------------------------------------------
+// funcao compila e pre-compila logicas ladder
+//----------------------------------------------
+function converte_sfc_ladder(){
+	}
