e01b2a38fa04f115377927e99fbce540b854b787
Giovani Batista de Souza <49683651+giovanisouza11@users.noreply.github.com>
2021-12-13 09:12:54 -0300

    Update appsfc.js

--- public/js_sfc/appsfc.js
+++ public/js_sfc/appsfc.js
@@ -16,9 +16,9 @@ function draw_sfc_inicio() {
 	canvassfc = document.getElementById("tela_sfc");
 	contextsfc = canvassfc.getContext("2d");
 	canvassfc.width = 600;
-	canvassfc.height = 60 * 1000; // Num_Linhas;
+	canvassfc.height = 60 * Num_Linhas;
 	draw_sfc_fundo(0);
-	//inicializa_array();
+	inicializa_array();
 }
 
 //=====================================================================================
@@ -125,7 +125,7 @@ function editar_sfc() {
 // Apos leitura do arquivo
 //=====================================================================================
 function draw_sfc(fileArr) {
-	
+	lArraySfc= {};
 }
 
 //=====================================================================================
