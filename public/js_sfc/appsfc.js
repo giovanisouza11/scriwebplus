60e4be6163a4aaf7c9c1e5e9fbf954f74289169d
Giovani Batista de Souza <49683651+giovanisouza11@users.noreply.github.com>
2021-12-14 09:35:55 -0300

    Update appsfc.js

--- public/js_sfc/appsfc.js
+++ public/js_sfc/appsfc.js
@@ -27,12 +27,7 @@ function draw_sfc_inicio() {
 function draw_sfc_fundo(tipo) {
 	for (var iy = 0; iy <(canvassfc.height-59); iy=iy+60) {
 		for (var ix = 0; ix <(canvassfc.width-59); ix=ix+60){
-		   	//var i_x = 100;
-			//var i_y = 100;
-			contextsfc.drawImage(AISfc, 60*tipo, 540, 60, 60, ix, iy, 60, 60);
-			//contextsfc.drawImage(AISfc, 60*tipo, 540, 60, 60, 0, 0, 60, 60);
-			//contextsfc.drawImage(AISfc, 60*tipo, 540, 60, 60, 100, 100, 60, 60);
-			//contextsfc.drawImage(AISfc, 60*tipo, 540, 60, 60, 300, 500, 60, 60);
+		   	contextsfc.drawImage(AISfc, 60*tipo, 540, 60, 60, ix, iy, 60, 60);
 		}
 	}
 }
