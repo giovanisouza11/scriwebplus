54bd851fcde502003afb8d870c86deb23a09a65d
Giovani Batista de Souza <49683651+giovanisouza11@users.noreply.github.com>
2021-12-23 12:51:11 -0300

    Update appsfc.js

--- public/js_sfc/appsfc.js
+++ public/js_sfc/appsfc.js
@@ -540,7 +540,7 @@ function entrada_input_sfc(event){
 				lArrayTransicao[indexTransicao*14+8] = tag;
 				contextSfc.fillStyle = 'white';
 				contextSfc.rect((estadoX0+((posicaoXSfc-estadoX0)/2)+1)*60, (estadoY0+((posicaoYSfc-estadoY0)/2)+1)*60+50, 30, 10);
-				contextSfc.fillStyle = 'cyan';
+				contextSfc.fillStyle = 'red';
 				contextSfc.fillText(tag,(estadoX0+((posicaoXSfc-estadoX0)/2)+1)*60, (estadoY0+((posicaoYSfc-estadoY0)/2)+1)*60+60);
 				ICampo.style.display = "none";
 				ICampo.style.top = "680px";
@@ -552,8 +552,8 @@ function entrada_input_sfc(event){
 				lArrayTransicao[indexTransicao*14+7] = tag;
 				contextSfc.fillStyle = 'white';
 				contextSfc.rect((estadoX0+((posicaoXSfc-estadoX0)/2)+1)*60, (estadoY0+((posicaoYSfc-estadoY0)/2)+1)*60+42, 20, 10);
-				contextSfc.fillStyle = 'cyan';
-				contextSfc.fillText(tag,(estadoX0+((posicaoXSfc-estadoX0)/2))*60, (estadoY0+((posicaoYSfc-estadoY0)/2)+1)*60+52);
+				contextSfc.fillStyle = 'red';
+				contextSfc.fillText(tag,(estadoX0+((posicaoXSfc-estadoX0)/2)+1)*60, (estadoY0+((posicaoYSfc-estadoY0)/2)+1)*60+52);
 				document.getElementById("label_input").innerHTML = "Condição 7:";
 				document.getElementById('input_ladder').value = lArrayTransicao[indexTransicao*14+8];
 				if (lArrayTransicao[indexTransicao*14+8] == undefined )
@@ -564,7 +564,7 @@ function entrada_input_sfc(event){
 				lArrayTransicao[indexTransicao*14+6] = tag;
 				contextSfc.fillStyle = 'white';
 				contextSfc.rect((estadoX0+((posicaoXSfc-estadoX0)/2)+1)*60, (estadoY0+((posicaoYSfc-estadoY0)/2)+1)*60+32, 20, 10);
-				contextSfc.fillStyle = 'cyan';
+				contextSfc.fillStyle = 'red';
 				contextSfc.fillText(tag,(estadoX0+((posicaoXSfc-estadoX0)/2)+1)*60, (estadoY0+((posicaoYSfc-estadoY0)/2)+1)*60+42);
 				document.getElementById("label_input").innerHTML = "Condição 6:";
 				document.getElementById('input_ladder').value = lArrayTransicao[indexTransicao*14+7];
@@ -576,7 +576,7 @@ function entrada_input_sfc(event){
 				lArrayTransicao[indexTransicao*14+5] = tag;
 				contextSfc.fillStyle = 'white';
 				contextSfc.rect((estadoX0+((posicaoXSfc-estadoX0)/2)+1)*60, (estadoY0+((posicaoYSfc-estadoY0)/2)+1)*60+22, 20, 10);
-				contextSfc.fillStyle = 'cyan';
+				contextSfc.fillStyle = 'red';
 				contextSfc.fillText(tag,(estadoX0+((posicaoXSfc-estadoX0)/2)+1)*60, (estadoY0+((posicaoYSfc-estadoY0)/2)+1)*60+32);
 				document.getElementById("label_input").innerHTML = "Condição 5:";
 				document.getElementById('input_ladder').value = lArrayTransicao[indexTransicao*14+6];
@@ -588,8 +588,8 @@ function entrada_input_sfc(event){
 				lArrayTransicao[indexTransicao*14+4] = tag;
 				contextSfc.fillStyle = 'white';
 				contextSfc.rect((estadoX0+((posicaoXSfc-estadoX0)/2)+1)*60, (estadoY0+((posicaoYSfc-estadoY0)/2)+1)*60+12, 20, 10);
-				contextSfc.fillStyle = 'cyan';
-				contextSfc.fillText(tag,(estadoX0+((posicaoXSfc-estadoX0+1)/2))*60, (estadoY0+((posicaoYSfc-estadoY0)/2))*60+22);
+				contextSfc.fillStyle = 'red';
+				contextSfc.fillText(tag,(estadoX0+((posicaoXSfc-estadoX0+1)/2)+1)*60, (estadoY0+((posicaoYSfc-estadoY0)/2)+1)*60+22);
 				document.getElementById("label_input").innerHTML = "Condição 4:";
 				document.getElementById('input_ladder').value = lArrayTransicao[indexTransicao*14+5];
 				if (lArrayTransicao[indexTransicao*14+5] == undefined )
@@ -600,7 +600,7 @@ function entrada_input_sfc(event){
 				lArrayTransicao[indexTransicao*14+3] = tag;
 				contextSfc.fillStyle = 'white';
 				contextSfc.rect((estadoX0+((posicaoXSfc-estadoX0)/2)+1)*60, (estadoY0+((posicaoYSfc-estadoY0)/2)+1)*60+02, 20, 10);
-				contextSfc.fillStyle = 'cyan';
+				contextSfc.fillStyle = 'red';
 				contextSfc.fillText(tag,(estadoX0+((posicaoXSfc-estadoX0)/2)+1)*60, (estadoY0+((posicaoYSfc-estadoY0)/2)+1)*60+12);
 				document.getElementById("label_input").innerHTML = "Condição 3:";
 				document.getElementById('input_ladder').value = lArrayTransicao[indexTransicao*14+4];
@@ -613,7 +613,7 @@ function entrada_input_sfc(event){
 				lArrayTransicao[indexTransicao*14+2] = tag;
 				contextSfc.fillStyle = 'white';
 				contextSfc.rect((estadoX0+((posicaoXSfc-estadoX0)/2)+1)*60, (estadoY0+((posicaoYSfc-estadoY0)/2)+1)*50, 20, 12);
-				contextSfc.fillStyle = 'cyan';
+				contextSfc.fillStyle = 'red';
 				contextSfc.fillText(tag,(estadoX0+((posicaoXSfc-estadoX0+1)/2)+1)*60, (estadoY0+((posicaoYSfc-estadoY0)/2)+1)*60+2);
 				document.getElementById("label_input").innerHTML = "Condição 2:";
 				document.getElementById('input_ladder').value = lArrayTransicao[indexTransicao*14+3];
