1a740ea24e25cdaeae88ea1161f689dc9bb4db74
Giovani Batista de Souza <49683651+giovanisouza11@users.noreply.github.com>
2021-12-27 17:05:21 -0300

    Update appsfc.js

--- public/js_sfc/appsfc.js
+++ public/js_sfc/appsfc.js
@@ -326,16 +326,16 @@ function draw_sfc(fileArr) {
 			contextSfc.fillText(fileLine[2],posicaoXSfc*60+2, posicaoYSfc*60+50);
 		}
 		if (tamanho_array_sfc ==21) {
-			sfcString = lArrayEstado[fileLine[0]*30];
-			indexSfcString = sfcString.indexOf(" ");
-			tamanhoSfcString = sfcString.length;
-			estadoY0 = sfcString.substr(0, indexSfcString);
-			estadoX0 = sfcString.substr(indexSfcString, tamanhoSfcString);
-			sfcString = lArrayEstado[fileLine[1]*30];
-			indexSfcString = sfcString.indexOf(" ");
-			tamanhoSfcString = sfcString.length;
-			posicaoYSfc = sfcString.substr(0, indexSfcString);
-			posicaoXSfc = sfcString.substr(indexSfcString, tamanhoSfcString);
+			numeroEstadoOrigem = lArrayEstado[fileLine[0]*30];
+			indexSfcString = numeroEstadoOrigem.indexOf(" ");
+			tamanhoSfcString = numeroEstadoOrigem.length;
+			estadoY0 = numeroEstadoOrigem.substr(0, indexSfcString);
+			estadoX0 = numeroEstadoOrigem.substr(indexSfcString, tamanhoSfcString);
+			numeroEstado = lArrayEstado[fileLine[1]*30];
+			indexSfcString = numeroEstado.indexOf(" ");
+			tamanhoSfcString = numeroEstado.length;
+			posicaoYSfc = numeroEstado.substr(0, indexSfcString);
+			posicaoXSfc = numeroEstado.substr(indexSfcString, tamanhoSfcString);
 			
 			draw_transicao(contextSfc, posicaoXSfc, posicaoYSfc, "black")
 			for(var j=0; j<tamanho_array_sfc; j++)
