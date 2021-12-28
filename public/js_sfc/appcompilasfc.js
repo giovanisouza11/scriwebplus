de044cbdaaa4bb96bb978cc6f46d2de1222a369e
Giovani Batista de Souza <49683651+giovanisouza11@users.noreply.github.com>
2021-12-28 11:32:09 -0300

    Update appcompilasfc.js

--- public/js_sfc/appcompilasfc.js
+++ public/js_sfc/appcompilasfc.js
@@ -1,6 +1,10 @@
 //-----------------------------------------------------------------------------------
 //Vari�veis utilizadas na convers�o
 //-----------------------------------------------------------------------------------
+// lArrayEstado = (LinhaColuna, 1-NUM_ESTADO,2-Memoria,3-Estado de chegada1,..,..,..,..,8-Estado de chegada5,
+//                 9-Estado_Destino 1,..,..,..,..,..,..,16-Estado_DEstino8,
+//                 17-Ação1,..,..,..,..,..,..,24-Ação 8,25-se estado 0 númerodememórias, 26-TAMANHO,27-Reserva,28-Reserva,29-Reserva
+// lArrayTransiçao = (Estado origem, 1-estado destino, 2-Condoção 1,..,..,..,..,..,8-Condição 7,9-ponto1,10-ponto2,11-ponto3,12-ponto4,13-ponto5,14ponto6,15-Res,16-Res,17-Res,18-Res,19-res)
 
 //=====================================================================================
 // Converte programa LADDER -> BOOLEANO
