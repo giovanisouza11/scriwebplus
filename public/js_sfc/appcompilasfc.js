1ad1e023a9b24ccb9a343bf1cb24d9f74e4ea912
Giovani Batista de Souza <49683651+giovanisouza11@users.noreply.github.com>
2021-12-28 16:30:00 -0300

    Update appcompilasfc.js

--- public/js_sfc/appcompilasfc.js
+++ public/js_sfc/appcompilasfc.js
@@ -17,7 +17,7 @@ function compila_sfc(){
 	//var booleano = new Array();
 	var index_str = 0;
 	while( si < (lArrayEstado.length/30)) {
-		/*for (int sj=0; sj<8; sj++){
+		for (var sj=0; sj<8; sj++){
 			if(lArrayEstado[si*30+17+sj] != "undefined"){
 				if ( lArrayTransicao[lArrayEstado[si*30+17+sj]] != (-1)) {
 					booleano[booleano.length] = "LD";
@@ -47,7 +47,7 @@ function compila_sfc(){
 				booleano[booleano.length] = "RST";
 			        booleano[booleano.length] = lArrayEstado[lArrayTransicao[lArrayEstado[si*30+17+sz]*20]+2];
 			}
-		}*/
+		}
 		si++;					
 	}
 }
