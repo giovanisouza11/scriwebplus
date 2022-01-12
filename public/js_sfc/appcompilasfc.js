41ce227308e95848df28c26b9482ff4796f255ca
Giovani Batista de Souza <49683651+giovanisouza11@users.noreply.github.com>
2022-01-12 09:09:37 -0300

    Update appcompilasfc.js

--- public/js_sfc/appcompilasfc.js
+++ public/js_sfc/appcompilasfc.js
@@ -349,25 +349,27 @@ function converte_sfc_ladder(){
 		switch (booleano[csl]){
 			case 'LD': 
 			    	// caso ser LD é um inicio de linha ou um fim de o opando "R"
-				if ((booleano[csl+1].charAt(0) == 'R') && ((booleano[csl+2] == 'SET') || (booleano[csl+2] == 'MOV') ){
-					flag = 1;
-					coluna = maximoColuna;
-					for(var csl1=linhaAnteriorSfc; csl1 < (linha); csl1++)
-						larray[csl1*8*9+maximoColuna*9+5] = 1;
-				     	maximoLinha = linha;
-				 	if (maximoColuna <6)
-					     for(var csl1=maximoColuna; csl1<7; csl1++)
-						     larray[linhaAnteriorSfc*8*9+csl1*9+3] = 11;
-					for(var csl1=linhaAnteriorSfc; csl1<linha; csl1++){
-						var maximoColunaAuxiliar = maximoColuna;
-						while ((larray[csl1*8*9+maximoColunaAuxiliar*9+3] < 1) && (larray[csl1*8*9+maximoColunaAuxiliar*9+3] > 11) && (maximoColunaAuxiliar > 0)) {
-							larray[csl1*8*9+maximoColunaAuxiliar*9+3] = 11;
-							maximoColunaAuxiliar--;
+				if ((booleano[csl+1].charAt(0) == 'R')) { // && ((booleano[csl+2] == 'SET') || (booleano[csl+2] == 'MOV') ){
+					if ((booleano[csl+2] == 'SET') || (booleano[csl+2] == 'MOV') ){
+						flag = 1;
+						coluna = maximoColuna;
+						for(var csl1=linhaAnteriorSfc; csl1 < (linha); csl1++)
+							larray[csl1*8*9+maximoColuna*9+5] = 1;
+				     		maximoLinha = linha;
+				 		if (maximoColuna <6)
+					     		for(var csl1=maximoColuna; csl1<7; csl1++)
+						     		larray[linhaAnteriorSfc*8*9+csl1*9+3] = 11;
+						for(var csl1=linhaAnteriorSfc; csl1<linha; csl1++){
+							var maximoColunaAuxiliar = maximoColuna;
+							while ((larray[csl1*8*9+maximoColunaAuxiliar*9+3] < 1) && (larray[csl1*8*9+maximoColunaAuxiliar*9+3] > 11) && (maximoColunaAuxiliar > 0)) {
+								larray[csl1*8*9+maximoColunaAuxiliar*9+3] = 11;
+								maximoColunaAuxiliar--;
+							}
 						}
+						linha = linhaAnteriorSfc;
+						maximoColuna= 0;
+						coluna = 7;
 					}
-					linha = linhaAnteriorSfc;
-					maximoColuna= 0;
-					coluna = 7;
 					csl++;
 			    	}
 			    	else {
