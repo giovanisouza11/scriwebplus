429ab60b44c9427df7ba9f25b266bf3fc1016a32
Giovani Batista de Souza <49683651+giovanisouza11@users.noreply.github.com>
2022-02-14 08:48:59 -0300

    Update appsfc.js

--- public/js_sfc/appsfc.js
+++ public/js_sfc/appsfc.js
@@ -877,7 +877,7 @@ function entrada_input_sfc(event){
 //=====================================================================================
 function posicao_sfc(transicao) {
 	var i =0;
-	var posSfcX = [0,15,25,35,45,0];
+	var posSfcX = [0,15,25,35,45,60];
 	var posSfcY = [45,60,60,60,60,45];
 	if (lArrayTransicao[transicao*20]>-1){
 		while (i<6) {
@@ -892,7 +892,7 @@ function posicao_sfc(transicao) {
 		resultado[0] = lArrayEstado[lArrayTransicao[transicao*20]*30].substring(indexSfcString+1, tamanhoSfcString)*60 + posSfcX[i-1];
 	}
 	
-	posSfcX = [0,0,15,25,35,45,0,0];
+	posSfcX = [0,0,15,25,35,45,60,60];
 	posSfcY = [30,15,0,0,0,0,15,30];
 	i = 0;
 	while (i<8) {
